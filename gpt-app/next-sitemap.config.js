a5cd4f86a06ad63c9485fcad553b07486db43abf
Богдан <brazbogdan@gmail.com>
2025-11-27 15:24:56 +0200

    Update dependencies

---
+++ gpt-app/next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: 'https://gp-titi.vercel.app',
+  generateRobotsTxt: true,
+  generateIndexSitemap: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  };
