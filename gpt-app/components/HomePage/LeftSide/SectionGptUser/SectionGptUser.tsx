abe8de491b87b1e4204604c6e8f2373d3af9f55b
IhorDid <igorbond31@gmail.com>
2026-04-29 21:16:33 +0300

    add paste img for chat

--- gpt-app/components/HomePage/LeftSide/SectionGptUser/SectionGptUser.tsx
+++ gpt-app/components/HomePage/LeftSide/SectionGptUser/SectionGptUser.tsx
@@ -55,7 +55,11 @@ export default function SectionGptUser() {
           </button>
         )}
       </div>
-      <LoginModal open={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
+      {/* <LoginModal open={isLoginOpen} onClose={() => setIsLoginOpen(false)} /> */}
+      <ErrorPatchImgModal
+        open={isLoginOpen}
+        onClose={() => setIsLoginOpen(false)}
+      />
     </section>
   );
 }
